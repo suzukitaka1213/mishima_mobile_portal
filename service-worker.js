d5517963d390446c17362672d6ab246a64d77d30
suzukitaka1213 <suzukitaka@pcdepot.jp>
2026-07-07 01:18:05 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'mishima-mobile-v1';
+const APP_ASSETS = [
+  './',
+  './mishima_mobile.html',
+  './manifest.webmanifest',
+  './mishima-mobile-icon-192.png',
+  './mishima-mobile-icon-512.png',
+  './mishima-mobile-maskable-512.png',
+  './apple-touch-icon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    )).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+
+      return fetch(event.request).then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        return response;
+      }).catch(() => caches.match('./mishima_mobile.html'));
+    })
+  );
+});
