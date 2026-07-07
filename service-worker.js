c47e532e3dad8b5dd59abc8e48763f64a93debe8
suzukitaka1213 <suzukitaka@pcdepot.jp>
2026-07-07 13:29:23 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,13 @@
 const CACHE_NAME = 'mishima-mobile-v1';
-const APP_ASSETS = [
-  './',
-  './mishima_mobile.html',
-  './manifest.webmanifest',
-  './mishima-mobile-icon-192.png',
-  './mishima-mobile-icon-512.png',
-  './mishima-mobile-maskable-512.png',
-  './apple-touch-icon.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(APP_ASSETS))
-      .then(() => self.skipWaiting())
-  );
+const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-    )).then(() => self.clients.claim())
-  );
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+  ).then(() => self.clients.claim()));
 });
-
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-
-  event.respondWith(
-    caches.match(event.request).then(cached => {
-      if (cached) return cached;
-
-      return fetch(event.request).then(response => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
-        return response;
-      }).catch(() => caches.match('./mishima_mobile.html'));
-    })
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request)).catch(() => caches.match('/index.html')));
 });
